8ec108572307e3c258458c7f2e23500338340cdb
Diogo Muniz <diogomuniz.web@gmail.com>
2024-03-17 18:55:26 -0300

    Adicionado script para automatizar execução dos comandos para iniciar serviços de mensageria

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Start Apache
+apache2-foreground &
+
+# Wait for Apache to start
+while ! pgrep -x httpd > /dev/null; do sleep 1; done
+
+# Execute PHP scripts
+cd /var/www/html/src/Mensageria/
+nohup php EnviarPedidosPagosParaCozinhaEEnviarEmailCliente.php > /dev/null 2>&1 &
+
+# Keep container running
+exec "$@"
